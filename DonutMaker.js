feeb8613bf47c7ee2ae0d406a06e854e6b5fbc89
MirandaPerry1982 <mirandaperry1982@yahoo.com>
2021-07-05 19:06:23 -0400

    Basic logic and HTML almost done

--- DonutMaker.js
+++ DonutMaker.js
@@ -1,24 +1,34 @@
 class DonutMaker {
-  constructor(numDonuts, numAutoClickers, numDonutMultipliers) {
+  constructor(numDonuts, numAutoClickers, numDonutMultipliers, autoClickCost, multiplierCost) {
     this.numDonuts = numDonuts;
     this.numAutoClickers = numAutoClickers;
     this.numDonutMultipliers = numDonutMultipliers;
+    this.autoClickCost = autoClickCost;
+    this.multiplierCost = multiplierCost;
   }
 
   addDonut(){
     this.numDonuts++;
+    if(this.numDonuts >= 100){
+      autoClickBtn.removeAttribute('disabled');
+    } else {
+        autoClickBtn.setAttribute('disabled', 'disabled');
+    }
+    
   }
 
   addAutoClicker() {
     if(this.numDonuts >= 100) {
       this.numAutoClickers++;
       this.numDonuts -= 100;
+      this.autoClickCost = Math.round(this.autoClickCost * 1.1);
     }
   }
 
   activateAutoClickers() {
     if (this.numAutoClickers > 0) {
       this.numDonuts += this.numAutoClickers * 1;
+
     }
   }
 
@@ -26,7 +36,17 @@ class DonutMaker {
     if(this.numDonuts >= 10){
       this.numDonutMultipliers++;
       this.numDonuts -= 10;
+      this.multiplierCost = Math.round(this.multiplierCost * 1.1);
+      this.numDonuts = Math.pow(1.2, this.numDonutMultipliers);
     }
+  }
+
+  reset(){
+    if(this.numDonuts >= 1){
+    location.reload();
+}
+    
+
   }
 
 
--- donut-maker-app.js
+++ donut-maker-app.js
@@ -1,23 +1,66 @@
 import DonutMaker from './DonutMaker.js';
 
-console.log("testing");
+const myDonutMaker = new DonutMaker(0,0,0,100,10);
+const donutBtn = document.getElementById("donutBtn");
+donutBtn.addEventListener(
+    'click', () => {
+      myDonutMaker.addDonut();
+      console.log(myDonutMaker.numDonuts)
+    document.getElementById("donutCount").innerHTML = "Number of Donuts: " + myDonutMaker.numDonuts; 
+    })
 
-// const donutStats = (stats) => {
-//     return ('Donuts: ' + stats.numDonuts + 'Auto Clickers: ' + stats.autoClickers 
-//     + 'Donut Multipliers: ' + stats.numDonutMultipliers);
-//     console.log(donutStats.stats);
-// };
+    let autoClickBtn = document.getElementById("autoClickBtn");
+    autoClickBtn.addEventListener(
+        'click', () => {
+          myDonutMaker.addAutoClicker();
+          console.log(myDonutMaker.numAutoClickers)
+        document.getElementById("autoCount").innerHTML = "Number of Auto Clickers: " + myDonutMaker.numAutoClickers; 
+        document.getElementById("donutCount").innerHTML = "Number of Donuts: " + myDonutMaker.numDonuts;
+        document.getElementById("autoClickCost").innerHTML = "Cost of Auto Clicker: " + myDonutMaker.autoClickCost;
+        myDonutMaker.activateAutoClickers();
+        console.log(myDonutMaker.numAutoClickers);
+
+        setInterval(function(){
+            myDonutMaker.activateAutoClickers(); 
+            document.getElementById("donutCount").innerHTML = "Number of Donuts: " + myDonutMaker.numDonuts;
+        }, 1000);
+         })
+
+    const multiplierBtn = document.getElementById("multiplierBtn");
+    multiplierBtn.addEventListener('click', ()=>{
+        myDonutMaker.addDonutMultiplier();
+        document.getElementById('multiplierCount').innerHTML = "Number of Multipliers: " + myDonutMaker.numDonutMultipliers;
+        document.getElementById('multiplierCost').innerHTML = "Cost of Multiplier: " + myDonutMaker.multiplierCost;
+        console.log(myDonutMaker.numDonutMultipliers)
+
+    }) 
+
+    const resetBtn = document.getElementById("resetBtn");
+    resetBtn.addEventListener('click', ()=>{
+        myDonutMaker.reset();
+
+    })
+    
 
 
 
 
-// const donutMaker = new DonutMaker(0,0,0);
-// const donutStatsPara = document.createElement('p');
-// donutStatsPara.innerText = donutStats(stats);
-// div.appendChild(donutStatsPara);
 
-// const makeDonutBtn = document.createElement('button');
-// makeDonutBtn.innerText = 'Make a Donut';
-// div.appendChild(makeDonutBtn);
+
+
+
+
+    
+
+
+
+        
+
+
+
+
+
+
+
 
 
